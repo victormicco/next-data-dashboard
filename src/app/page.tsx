3e5e86ee68f9307fe7fabf8e89b3eaa720690732
Victor Micco <victor.oficial093@gmail.com>
2023-10-04 18:52:50 -0300

    auth issue not fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import styles from "./page.module.css";
 import Dashboard from "./dashboard/page";
-import Header from "./components/Header";
-import SideMenu from "./components/SideMenu/SideMenu";
+import Header from "@/components/Header";
+import SideMenu from "@/components/SideMenu";
+import Login from "../components/Login/page";
+import SessionProvider from "../components/SessionProvider/_app";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
 			<Header />
 			<SideMenu />
 			<Dashboard />
+			<Login />
 		</main>
 	);
 }
